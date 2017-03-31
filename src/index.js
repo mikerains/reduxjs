a8bf2e6ed21c23da254b5e796c7ad0435a0ba5ab
Rains <Michael_Rains@gallup.com>
2017-03-30 22:57:05 -0500

    rename fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import mytest from './test/mytest';
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
-mytest();
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
